d39550feeaacdce9fbf5de60e7156c4315df83f3
toast <toast@toast-server.net>
2024-04-26 20:13:58 +1000

    Fix poisoned uptime command

--- src/internals/utils.rs
+++ src/internals/utils.rs
@@ -4,7 +4,12 @@ use tokenservice_client::TokenServiceApi;
 pub static EMBED_COLOR: i32 = 0x5a99c7;
 
 pub static BOT_VERSION: Lazy<String> = Lazy::new(|| {
-  let cargo_version = cargo_toml::Manifest::from_path("Cargo.toml").unwrap().package.unwrap().version.unwrap();
+  let cargo_version = cargo_toml::Manifest::from_path(std::env::var("CARGO_MANIFEST_DIR").unwrap()+"/Cargo.toml")
+    .unwrap()
+    .package
+    .unwrap()
+    .version
+    .unwrap();
   format!("v{}", cargo_version)
 });
 
