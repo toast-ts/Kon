cf8caf1aaeb6c0345099c14a054a84ac7265e520
toast <toast@toast-server.net>
2024-08-20 11:50:32 +1000

    Remove cache layer

--- src/internals/tsclient.rs
+++ src/internals/tsclient.rs
@@ -1,11 +1,7 @@
-use std::sync::LazyLock;
-use tokio::sync::RwLock;
 use tokenservice_client::{
   TokenService,
   TokenServiceApi
 };
-
-static TS_GLOBAL_CACHE: LazyLock<RwLock<Option<TokenServiceApi>>> = LazyLock::new(|| RwLock::new(None));
 
 pub struct TSClient(TokenService);
 
@@ -17,17 +13,8 @@ impl TSClient {
   }
 
   pub async fn get(&self) -> Result<TokenServiceApi, crate::Error> {
-    {
-      let cache = TS_GLOBAL_CACHE.read().await;
-      if let Some(ref api) = *cache {
-        return Ok(api.clone());
-      }
-    }
-
     match self.0.connect().await {
       Ok(api) => {
-        let mut cache = TS_GLOBAL_CACHE.write().await;
-        *cache = Some(api.clone());
         Ok(api)
       }
       Err(e) => Err(e)
